97541ec317b10586aa04326e6a7354e853623b62
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-07 23:02:06 +0530

    Create cin_cout.cpp

---
+++ concepts/cin_cout.cpp
@@ -0,0 +1,39 @@
+
+// take the input form the user about the 
+
+
+#include<iostream>
+#include<string>
+
+int main()
+{
+    std::string name1, address1, phoneNo1;
+    std::string name2, address2, phoneNo2;
+
+    //get user1 information
+    std::cout<<"User1 what is your name?\n";
+    std::getline(std::cin, name1);
+    std::cout<<"User1 what is your address?\n";
+    std::getline(std::cin, address1);
+    std::cout<<"User1 what is your phone number?\n";
+    std::getline(std::cin, phoneNo1);
+
+    //get user2 information
+    std::cout<<"User2 what is your name?\n";
+    std::getline(std::cin, name2);
+    std::cout<<"User2 what is your address?\n";
+    std::getline(std::cin, address2);
+    std::cout<<"User2 what is your phone number?\n";
+    std::getline(std::cin, phoneNo2);
+
+    //print information
+    std::cout<<"\n\n"<<name1<<"\n";
+    std::cout<<"\t\t"<<address1<<"\n";
+    std::cout<<"\t\t"<<phoneNo1<<"\n";
+
+    std::cout<<"\n\n"<<name2<<"\n";
+    std::cout<<"\t\t"<<address2<<"\n";
+    std::cout<<"\t\t"<<phoneNo2<<"\n";    
+    return 0;
+}
+
