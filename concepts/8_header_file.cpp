84d52a626236ef98669dbf1cbd3407483ec2996f
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-07 19:29:21 +0530

    Create 8_header_file.cpp

---
+++ concepts/8_header_file.cpp
@@ -0,0 +1,35 @@
+/*
+ Header file contains information about how to do a task
+ Main program contains information about what to do to complete a task
+
+*/
+
+
+// Generic format 
+
+#include <iostream>
+using  namespace std ;
+
+int main()
+{
+cout<<"Hello, how are you";
+return 0 ;
+
+}
+
+// Using header files 
+/*
+# include "main.hpp"
+
+int main()
+{
+ cout << "hello, how are you";
+ return 0;
+
+}
+
+contents of main.hpp would be 
+
+#include <iostream>
+
+using namespace std ;
