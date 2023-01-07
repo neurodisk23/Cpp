d401fbb920d483cdb6792c1ee7792a7034b8a13b
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-08 01:04:44 +0530

    Create 03_variable_types_1.cpp

---
+++ 03_arithmetic_operations/03_variable_types_1.cpp
@@ -0,0 +1,13 @@
+// Terminal will throw 0 
+
+     #include <iostream>
+
+     int main(void)
+     {
+         int numerator = 4;
+         int denominator = 5;
+
+         int answer = numerator / denominator;
+         std::cout<<"answer = "<<answer;
+         return 0;
+     }
