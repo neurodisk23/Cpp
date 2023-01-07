920e6f0a7a481679dd136d78021b6e85c006c055
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-07 16:43:19 +0530

    Create Defining_contants.cpp

---
+++ concepts/Defining_contants.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int weightGoal = 100;
+    cout<<"WeightGoal = "<<weightGoal<<"\n";
+    weightGoal = 200;
+    cout<<"WeightGoal = "<<weightGoal<<"\n";
+    return 0;
+}
