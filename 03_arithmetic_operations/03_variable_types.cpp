164baa53f7b884c1ae91745af7c3d98ae5002c93
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-08 00:55:31 +0530

    Create 03_variable_types.cpp

---
+++ 03_arithmetic_operations/03_variable_types.cpp
@@ -0,0 +1,13 @@
+/*4.5 * 5.5 = 24.75 The program outputs 24, when the correct answer is 24.75. */
+
+     #include <iostream>
+
+     int main()
+     {
+          float width = 4.5;
+          float height = 5.5;
+
+          int area = width * height;
+          std::cout << "area = " << area;
+          return 0;
+     }
