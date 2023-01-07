b32aadab3aa473899af3465eefe257457cd15a63
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-08 01:08:13 +0530

    Update 03_variable_types_1.cpp

--- 03_arithmetic_operations/03_variable_types_1.cpp
+++ 03_arithmetic_operations/03_variable_types_1.cpp
@@ -11,3 +11,27 @@
          std::cout<<"answer = "<<answer;
          return 0;
      }
+
+
+/*
+
+/*Goal: fix the variable types problem. 
+**This program outputs the wrong answer
+**even though it compiles and executes without errors. 
+**Fix it so that it outputs the correct value.
+*/
+
+#include <iostream>
+int main(void)
+{
+    float numerator = 4;
+    float denominator = 5;
+
+    float answer = numerator / denominator;
+    std::cout<<"answer = "<<answer;
+    return 0;
+}
+
+
+
+*/
