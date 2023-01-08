fec41976524e6632621bece376708f34e2086c0e
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-08 10:23:57 +0530

    Create 11_for_loop_problem.cpp

---
+++ 04_Control_flow/11_for_loop_problem.cpp
@@ -0,0 +1,28 @@
+
+/*Write a program that asks a user for five numbers.
+**Print out the sum and average of the five numbers.
+*/
+
+// SOLUTION
+
+/*
+
+#include <iostream>
+
+int main()
+{   float number,sum = 0 ;
+
+    for (int i = 0; i<5 ;i++){
+        std::cout<<"Please input the "<<i+1<<"number";
+        std::cin>>number;
+        sum +=number ;
+    }
+    
+    std::cout<<"The sum is "<<sum<<"\n";
+    std::cout<<"The average is "<<sum/5<<"\n" ;
+    
+    
+    return 0;
+}
+
+*/
