f6647f8a1f6258732cc310263e84fcb57357df04
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-09 21:37:09 +0530

    Create 02_function_problem.cpp

---
+++ 07_Functions/02_function_problem.cpp
@@ -0,0 +1,21 @@
+/*
+
+The function should print the message, "Functions". Print this message with the same capitalization and punctuation, the grader will be checking for them.
+
+*/
+
+#include<iostream>
+
+void printMessage();
+
+int main()
+{
+
+    printMessage();
+    return 0;
+}
+
+void printMessage()
+{
+    std::cout<<"Functions";
+}
