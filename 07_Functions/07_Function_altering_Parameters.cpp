2674e26c8069bb114a03e8563869303bb09e3128
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-09 23:01:34 +0530

    Update 07_Function_altering_Parameters.cpp

--- 07_Functions/07_Function_altering_Parameters.cpp
+++ 07_Functions/07_Function_altering_Parameters.cpp
@@ -28,3 +28,24 @@ void increment(int input)
 
 //Solution : a is not incrementing in the main
 
+
+
+/*    IMPROVEMENT    */
+
+#include<iostream>
+
+int increment(int input);
+int main()
+{
+    int a = 34;
+    std::cout<<"Before the function call a = "<<a<<"\n";
+    a = increment(a);
+    std::cout<<"After the function call a = "<<a<<"\n";
+    return 0;
+}
+int increment(int input)
+{
+    input++;
+    std::cout<<"In the function call a = "<<input<<"\n";
+    return input;
+}
