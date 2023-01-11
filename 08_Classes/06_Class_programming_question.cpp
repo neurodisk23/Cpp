a020bbd46c359cf47422beb1a91f092ae59a1ec7
Harshal Verma <88970103+neurodisk23@users.noreply.github.com>
2023-01-11 23:39:50 +0530

    Create 06_Class_programming_question.cpp

---
+++ 08_Classes/06_Class_programming_question.cpp
@@ -0,0 +1,69 @@
+/*Goal: practice creating and using a class
+** Use the class 'Dog' to create to 
+** instances of the class, dog1 and dog2.
+** Assign dog1 the name: Trixie
+** Assign dog2 the name: Kali
+** Assign dog1 the license #1234
+** Assign dog2 the license #5678
+** Print the information for each dog.
+*/
+
+#include"main.hpp"
+
+int main()
+{
+    Dog dog1, dog2;
+    dog1.setName("Trixie");
+    dog2.setName("Kali");
+    dog1.setLicenseNumber(1234);
+    dog2.setLicenseNumber(5678);
+
+    dog1.printInfo();
+    cout<<"\n";
+    dog2.printInfo();
+    return 0;
+}
+
+/*The header file for main.cpp*/
+
+#include<iostream>
+
+using namespace std;
+
+class Dog
+{
+    string name;
+    int licenseNumber;
+public:
+    void setName(string nameIn);
+    void setLicenseNumber(int licenseNumberIn);
+    string getName();
+    int getLicenseNumber();
+    void printInfo();
+};
+
+void Dog::setName(string nameIn)
+{
+    name = nameIn;
+}
+
+void Dog::setLicenseNumber(int licenseNumberIn)
+{
+    licenseNumber = licenseNumberIn;
+}
+
+string Dog::getName()
+{
+    return name;
+}
+
+int Dog::getLicenseNumber()
+{
+    return licenseNumber;
+}
+
+void Dog::printInfo()
+{
+    cout<<name<<" "<<licenseNumber;
+}
+
